0a476443c8e8b694c7e849610f89e2a3db02a8c3
Stefano Vozza <svozza@gmail.com>
2016-03-10 21:58:54 +0000

    add implementation

--- python/RomanNumerals.py
+++ python/RomanNumerals.py
@@ -1,9 +1,11 @@
 from enum import Enum
 
 class Numerals(Enum):
+    __order__ = 'M CM CD D C XC L XL X IX V IV I'
     M = 1000
     CM = 900
     D = 500
+    CD = 400
     C = 100
     XC = 90
     L = 50
@@ -15,7 +17,21 @@ class Numerals(Enum):
     I = 1
 
 def convert(n):
-    return "MMMMCMXCIX"
+    if n > 5000:
+        raise Exception("Numbers of 5000 and over not supported")
+    if n <= 0:
+        raise Exception("Numbers of 0 and below not supported")
+    result = ""
+    for rm in Numerals:
+        while n - rm.value >= 0:
+            result += rm.name
+            n -= rm.value
+    return result
+
+try:
+    convert(5001)
+except Exception, ex:
+    assert str(ex) == "Numbers of 5000 and over not supported"
 
 assert convert(4999) == "MMMMCMXCIX"
 assert convert(4000) == "MMMM"
@@ -35,10 +51,20 @@ assert convert(19) == "XIX"
 assert convert(14) == "XIV"
 assert convert(13) == "XIII"
 assert convert(9) == "IX"
-assert convert(8) == "III"
+assert convert(8) == "VIII"
 assert convert(5) == "V"
 assert convert(3) == "III"
 assert convert(2) == "II"
 assert convert(1) == "I"
+
+try:
+    convert(0)
+except Exception, ex:
+    assert str(ex) == "Numbers of 0 and below not supported"
+
+try:
+    convert(-1)
+except Exception, ex:
+    assert str(ex) == "Numbers of 0 and below not supported"
 
 
